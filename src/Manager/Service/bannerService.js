0e35eee2a155013099330affd391657e054927ac
Onur Çöpoğlu <100149612+onurcpgl@users.noreply.github.com>
2023-02-18 19:06:52 +0300

    Banner page update and delete

--- src/Manager/Service/bannerService.js
+++ src/Manager/Service/bannerService.js
@@ -1,25 +1,25 @@
-import axiosInstance from "../../Middleware/jwtInterceptor";
+import axiosInstance from '../../Middleware/jwtInterceptor'
 
 const fetchBanners = async () => {
-  const result = await axiosInstance.get("banners");
-  return result.data;
-};
+  const result = await axiosInstance.get('banners')
+  return result.data
+}
 const getByCode = async (code) => {
-  const result = await axiosInstance.get(`banners/${code}`);
-  return result;
-};
+  const result = await axiosInstance.get(`banners/${code}`)
+  return result
+}
 const addBanners = async (bannerDto) => {
-  var result = await axiosInstance.post("banners", bannerDto);
-  return result;
-};
+  var result = await axiosInstance.post('banners', bannerDto)
+  return result
+}
 const updateBanners = async (bannerDto) => {
-  const result = await axiosInstance.put("banners", bannerDto);
-  return result;
-};
+  const result = await axiosInstance.put('banners', bannerDto)
+  return result
+}
 const deleteBanners = async (code) => {
-  const result = await axiosInstance.delete(`banners/${code}`);
-  return result;
-};
+  const result = await axiosInstance.delete(`banners/${code}`)
+  return result
+}
 
 const exportFunction = {
   fetchBanners,
@@ -27,6 +27,6 @@ const exportFunction = {
   updateBanners,
   getByCode,
   deleteBanners,
-};
+}
 
-export default exportFunction;
+export default exportFunction
